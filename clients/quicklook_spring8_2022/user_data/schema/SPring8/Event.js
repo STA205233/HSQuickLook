0237efd752712d421076116116f3364c316e5eaa
STA205233 <93177954+STA205233@users.noreply.github.com>
2022-11-01 04:04:41 +0900

    Add some figures

--- clients/quicklook_spring8_2022/user_data/schema/SPring8/Event.js
+++ clients/quicklook_spring8_2022/user_data/schema/SPring8/Event.js
@@ -1,7 +1,7 @@
 HSQuickLook.main.schema =
 	[
 		{
-			"collection": "SPring8_2022",
+			"collection": "Scalardata",
 			"directory": "Detector",
 			"document": "Event",
 			"period": "1",
@@ -15,7 +15,7 @@ HSQuickLook.main.schema =
 			}
 		},
 		{
-			"collection": "SPring8_2022",
+			"collection": "Scalardata",
 			"directory": "Detector",
 			"document": "Event",
 			"period": "1",
@@ -24,5 +24,37 @@ HSQuickLook.main.schema =
 				"Temperature": { "type": "int" },
 				"Temperature-graph": { "type": "trend-graph", "group": [{ "source": "Temperature", "options": { "legend": "Temperature" } }], "options": { "xWidth": 100, "ywidth": 20, } },
 			}
+		},
+		{
+			"collection": "Images",
+			"directory": "Detector",
+			"document": "Event_weight",
+			"period": "1",
+			"section": "PushToQuickLookDB_HistogramXrayEventProperties_weight",
+			"contents": {
+				"weight": { "type": "image" }
+			}
+		},
+		{
+			"collection": "Images",
+			"directory": "Detector",
+			"document": "Event_sumPH",
+			"period": "1",
+			"section": "PushToQuickLookDB_HistogramXrayEventProperties_sumPH",
+			"contents": {
+				"sumPH": { "type": "image" }
+			}
+		},
+		{
+			"collection": "Images",
+			"directory": "Detector",
+			"document": "Event_pixel",
+			"period": 1,
+			"section": "PushToQuickLookDB_HistogramXrayEventProperties_pixels",
+			"contents": {
+				"ix": { "type": "image" },
+				"iy": { "type": "image" }
+			}
+
 		}
 	];
